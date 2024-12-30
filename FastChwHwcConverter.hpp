b7f4f092ed4f13a901e6e677ec5b3a9879345a8e
張小凡 <2672931+whyb@users.noreply.github.com>
2024-12-30 18:28:59 +0800

    Implementing a single-threaded version

---
+++ FastChwHwcConverter.hpp
@@ -0,0 +1,140 @@
+#pragma once
+
+#include <array>
+#include <algorithm>
+#include <cmath>
+#include <functional>
+#include <limits>
+
+namespace whyb {
+
+/**
+ * @brief Determines if two numbers are approximately equal
+ *
+ * @tparam Type Type of the numbers
+ * @param a First number
+ * @param b Second number
+ * @return true if the numbers are approximately equal
+ * @return false if the numbers are not approximately equal
+ */
+template <typename Type>
+inline bool is_number_equal(Type& a, Type& b) {
+    static Type epsilon = std::numeric_limits<Type>::epsilon();
+    return std::abs(a - b) < epsilon;
+}
+
+
+/**
+ * @brief Converts image data from HWC format to CHW format
+ *
+ * @tparam Stype Source data type
+ * @tparam Dtype Destination data type
+ * @param ch Number of channels
+ * @param w Image width
+ * @param h Image height
+ * @param src Pointer to the source data in HWC format
+ * @param dst Pointer to the destination data in CHW format
+ * @param alpha Scaling factor
+ * @param clamp Whether to clamp the data values
+ * @param min_v Minimum value for clamping
+ * @param max_v Maximum value for clamping
+ * @param normalized_mean_stds Whether to use mean and standard deviation for normalization
+ * @param mean Array of mean values for normalization
+ * @param stds Array of standard deviation values for normalization
+ */
+template <typename Stype, typename Dtype>
+inline void hwc2chw(
+    const size_t ch, const size_t w, const size_t h,
+    const Stype* src, Dtype* dst,
+    const Dtype alpha = 1, const bool clamp = false,
+    const Dtype min_v = 0.0, const Dtype max_v = 1.0,
+    const bool normalized_mean_stds = false,
+    const std::array<float, 3> mean = { 0.485, 0.456, 0.406 },
+    const std::array<float, 3> stds = { 0.229, 0.224, 0.225 }) {
+    std::function<Dtype(Stype)> cvt_fun;
+    if(clamp) {
+        if(is_number_equal<Dtype>(alpha, 1)) {
+            if(normalized_mean_stds) {
+                cvt_fun = [&alpha,&min_v,&max_v,&mean,&stds](Stype& src_val, size_t& c){return static_cast<Dtype>(std::clamp<Dtype>((src_val - mean[c]) / stds[c], min_v, max_v));};
+            } else {
+                cvt_fun = [&alpha,&min_v,&max_v](Stype& src_val){return static_cast<Dtype>(std::clamp<Dtype>(src_val, min_v, max_v));};
+            }
+        } else {
+            if(normalized_mean_stds) {
+                cvt_fun = [&alpha,&min_v,&max_v,&mean,&stds](Stype& src_val, size_t& c){return static_cast<Dtype>(std::clamp<Dtype>((src_val * alpha - mean[c]) / stds[c], min_v, max_v));};
+            } else {
+                cvt_fun = [&alpha,&min_v,&max_v](Stype& src_val){return static_cast<Dtype>(std::clamp<Dtype>(src_val * alpha, min_v, max_v));};
+            }
+        }
+    } else {
+        if(is_number_equal<Dtype>(alpha, 1)) {
+            if(normalized_mean_stds) {
+                cvt_fun = [&alpha,&mean,&stds](Stype& src_val, size_t& c){return static_cast<Dtype>((src_val - mean[c]) / stds[c]);};
+            } else {
+                cvt_fun = [&alpha](Stype& src_val){return static_cast<Dtype>(src_val);};
+            }
+        } else {
+            if(normalized_mean_stds) {
+                cvt_fun = [&alpha,&mean,&stds](Stype& src_val, size_t& c){return static_cast<Dtype>((src_val * alpha - mean[c]) / stds[c]);};
+            } else {
+                cvt_fun = [&alpha](Stype& src_val){return static_cast<Dtype>(src_val * alpha);};
+            }
+        }
+    }
+    size_t index = 0UL;
+    const size_t hw_stride = w * h;
+    for (size_t s = 0UL; s < hw_stride; ++s) {
+        size_t stride_index = s;
+        for (size_t c = 0UL; c < ch; ++c, stride_index += hw_stride) {
+            dst[stride_index] = cvt_fun(src[index++]);
+        }
+    }
+}
+
+
+/**
+ * @brief Converts image data from CHW format to HWC format
+ *
+ * @tparam Stype Source data type
+ * @tparam Dtype Destination data type
+ * @param ch Number of channels
+ * @param w Image width
+ * @param h Image height
+ * @param src Pointer to the source data in CHW format
+ * @param dst Pointer to the destination data in HWC format
+ * @param alpha Scaling factor
+ * @param clamp Whether to clamp the data values
+ * @param min_v Minimum value for clamping
+ * @param max_v Maximum value for clamping
+ */
+template <typename Stype, typename Dtype>
+inline void chw2hwc(
+    const size_t ch, const size_t w, const size_t h, 
+    const Stype* src, Dtype* dst, 
+    const double alpha = 1, const bool clamp = false,
+    const Dtype min_v = 0, const Dtype max_v = 255) {
+    std::function<Dtype(Stype)> cvt_fun;
+    if(clamp) {
+        if(is_number_equal<Dtype>(alpha, 1)) {
+            cvt_fun = [&alpha,&min_v,&max_v](Stype& src_val){return static_cast<Dtype>(std::clamp<Dtype>(src_val * alpha, min_v, max_v));};
+        } else {
+            cvt_fun = [&alpha,&min_v,&max_v](Stype& src_val){return static_cast<Dtype>(std::clamp<Dtype>(src_val, min_v, max_v));};
+        }
+    } else {
+        if(is_number_equal<Dtype>(alpha, 1)) {
+            cvt_fun = [&alpha](Stype& src_val){return static_cast<Dtype>(src_val);};
+        } else {
+            cvt_fun = [&alpha](Stype& src_val){return static_cast<Dtype>(src_val * alpha);};
+        }
+    }
+    size_t index = 0UL;
+    const size_t hw_stride = w * h;
+    for (size_t s = 0UL; s < hw_stride; ++s) {
+        size_t stride_index = s;
+        for (size_t c = 0UL; c < ch; ++c, stride_index += hw_stride) {
+            dst[index++] = cvt_fun(src[stride_index]);
+        }
+    }
+}
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,107 @@
 # FastChwHwcConverter
-A high-performance, header-only C++ library for image tensor data format conversion, leveraging OpenMP parallel processing for lightning-fast performance.
+
+## Overview
+FastChwHwcConverter is a high-performance, header-only C++ library for converting image data formats between HWC (Height, Width, Channels) and CHW (Channels, Height, Width). The library leverages OpenMP parallel processing to provide lightning-fast performance.
+
+## Features
+- **High-Performance**: Utilizes OpenMP for parallel processing. Make full use of CPU multi-core features.
+- **Header-Only**: Easy to integrate into your C/C++ project.
+- **Flexible**: Supports scaling, clamping, and normalization of image data.
+
+## Installation
+Simply include the header file `FastChwHwcConverter.hpp` in your project:
+
+```cpp
+#include "FastChwHwcConverter.hpp"
+```
+
+## Requirements
+* C++11 or later
+* OpenMP support (optional but recommended for high performance)
+
+## Usage
+
+### HWC to CHW Conversion
+The `hwc2chw` function converts image data from HWC format to CHW format.
+```cpp
+template <typename Stype, typename Dtype>
+void hwc2chw(
+    const size_t ch, const size_t w, const size_t h,
+    const Stype* src, Dtype* dst,
+    const Dtype alpha = 1, const bool clamp = false,
+    const Dtype min_v = 0.0, const Dtype max_v = 1.0,
+    const bool normalized_mean_stds = false,
+    const std::array<float, 3> mean = { 0.485, 0.456, 0.406 },
+    const std::array<float, 3> stds = { 0.229, 0.224, 0.225 }
+);
+```
+
+Parameters:
+
+* `ch`: Number of channels.
+* `w`: Width of the image.
+* `h`: Height of the image.
+* `src`: Pointer to the source data in HWC format.
+* `dst`: Pointer to the destination data in CHW format.
+* `alpha`: Scaling factor (default is 1).
+* `clamp`: Whether to clamp the data values (default is false).
+* `min_v`: Minimum value for clamping (default is 0.0).
+* `max_v`: Maximum value for clamping (default is 1.0).
+* `normalized_mean_stds`: Whether to use mean and standard deviation for normalization (default is false).
+* `mean`: Array of mean values for normalization (default is {0.485, 0.456, 0.406}).
+* `stds`: Array of standard deviation values for normalization (default is {0.229, 0.224, 0.225}).
+
+### CHW to HWC Conversion
+The `chw2hwc` function converts image data from CHW format to HWC format.
+
+```cpp
+template <typename Stype, typename Dtype>
+void chw2hwc(
+    const size_t ch, const size_t w, const size_t h, 
+    const Stype* src, Dtype* dst, 
+    const double alpha = 1, const bool clamp = false,
+    const Dtype min_v = 0, const Dtype max_v = 255
+);
+```
+Parameters:
+
+* `ch`: Number of channels.
+* `w`: Width of the image.
+* `h`: Height of the image.
+* `src`: Pointer to the source data in CHW format.
+* `dst`: Pointer to the destination data in HWC format.
+* `alpha`: Scaling factor (default is 1).
+* `clamp`: Whether to clamp the data values (default is false).
+* `min_v`: Minimum value for clamping (default is 0).
+* `max_v`: Maximum value for clamping (default is 255).
+
+
+### Example
+```cpp
+#include "FastChwHwcConverter.hpp"
+
+int main() {
+    const size_t ch = 3;
+    const size_t w = 1920;
+    const size_t h = 1080;
+    uint8_t src_uint8[h * w * ch]; // Source data(hwc)
+    float src_float[h * w * ch]; // Source data(chw)
+
+    float out_float[ch * w * h]; // Inference output data(chw)
+    uint8_t out_uint8[ch * w * h]; // Inference output data(hwc)
+
+    // Fill src with example data...
+
+    whyb::hwc2chw<uint8_t, float>(ch, w, h, src_uint8, src_float);
+
+    // Do AI inference ...
+
+    whyb::chw2hwc(ch, w, h, out_float, out_uint8);
+    // Done
+
+    return 0;
+}
+```
+
+## Contact
+For any questions or suggestions, please open an issue or contact the maintainer.
